3c628f274413b5ad7f355fcbcc1a9a17ad054954
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-12-18 16:16:25 +0000

    workspace redirect link - /truebalance

--- truebalance/truebalance/page/bank_reconciliation/bank_reconciliation.js
+++ truebalance/truebalance/page/bank_reconciliation/bank_reconciliation.js
@@ -1,14 +1,14 @@
 frappe.pages['bank-reconciliation'].on_page_load = function (wrapper) {
-	// Redirect to the mint web app
-	window.location.href = '/mint';
+	// Redirect to the truebalance web app
+	window.location.href = '/truebalance';
 
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: __("Bank Reconciliation - Mint"),
+		title: __("Bank Reconciliation - TrueBalance"),
 		single_column: true,
 	});
 
 	page.set_primary_action("Open Bank Reconciliation", function () {
-		window.location.href = '/mint';
+		window.location.href = '/truebalance';
 	});
 }
